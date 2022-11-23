3803153ca0edff600418b044ddc9a08f04311e3f
spselenim <pawal.satish@gmail.com>
2022-11-23 21:48:11 +0530

    23th II

--- dec2022/src/test/java/dec2022/dec2022_simpleGit.java
+++ dec2022/src/test/java/dec2022/dec2022_simpleGit.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class dec2022_simpleGit {
   @Test
 	public void f() {
-	  System.out.println("Test235");//User1 : 21:35 // User 2: 21:47 // User2:23th Nov 21:41
+	  System.out.println("Test235");//User1 : 21:35 // User 2: 21:47 // User2:23th Nov 21:41 :01
   }
 }
